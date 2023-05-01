b9ef1ecad8918854428f333a549ed88ae85bbbd9
Gaurav Dubey <rmsd2804@gmail.com>
2023-05-01 20:12:42 +0530

    changes

---
+++ lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+Color Bg = Color(0xFF151515);
+Color primaryColor = Color(0xFF232327);
+Color secondaryColor = Color.fromARGB(255, 252, 252, 252);
+Color textWhite = Color.fromARGB(255, 255, 255, 255);
+Color textLight1 = Color(0xFFE1E1E1);
+Color textLight2 = Color(0xFFC4C4C4);
+Color Button = Color(0xFFFFCB11);
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:ecommerceui/pages/mainpage.dart';
+import 'package:ecommerceui/pages/navbar.dart';
 import 'package:flutter/material.dart';
-
 import 'pages/home_page.dart';
 
 void main() {
   runApp(MaterialApp(
-    initialRoute: 'categories',
+    debugShowCheckedModeBanner: false,
+    initialRoute: 'navbar',
     routes: {
       'home': (context) => HomePage(),
       'categories': (context) => MainPage(),
+      'navbar': (context) => NavBar(),
     },
   ));
 }
